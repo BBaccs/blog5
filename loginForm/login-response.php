c8fc591cb27a7f93cd62325188e2595ca854cf2a
Brian Baccarella <baccarellab@gmail.com>
2018-11-14 09:57:43 -0500

    add non db pw logic

--- loginForm/login-response.php
+++ loginForm/login-response.php
@@ -15,4 +15,14 @@ if (trim($userName) == "Jimmy") {
 
 
 
+//We would prolly need a database but without one here is how we could do password
+//if (trim($userName) == "Jimmy") && ($passWord) == "secret" {
+  //$_SESSION['isLoggedIn'] = true;
+  //header('Location: protected-page.php');
+//} else {
+  //header('Location: index?BC=true.php');
+//}
+
+
+
 ?>
